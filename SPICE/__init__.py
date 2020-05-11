ce67563a47cf8d6d9833934f2237646b8147ff79
HaleySchuhl <haleyschuhl@gmail.com>
2020-05-11 15:56:47 -0500

    huge restructure commit

--- SPICE/__init__.py
+++ SPICE/__init__.py
@@ -1,5 +1,2 @@
-from .SPICE import SPICE
-from .SPICE import SPICEParameters
-from .SPICE import unmix_cvxopt
-from .SPICE import unmix_qpp
-from .QPP import quadprog_solve_qp
+from SPICE import *
+from util import *
